4878c1a721119ac556bedb8222c43cf31440214b
hadighany25 <hadighany25@gmail.com>
2026-07-29 09:32:20 +0700

    Fix

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,6 +2,12 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
+    // បន្ថែមបន្ទាត់នេះ ដើម្បីឆែកមើលតម្លៃពិតប្រាកដនៅលើ Server
+    console.log(
+      "Check MONGO_URI on Server:",
+      process.env.MONGO_URI ? "✅ មានតម្លៃ" : "❌ ទទេ (Undefined)",
+    );
+
     await mongoose.connect(process.env.MONGO_URI);
     console.log("✅ MongoDB Connected Successfully!");
   } catch (error) {
